ee8515a4069df3a960df5e41b27d92090f9884fa
aashish.biradar <aashish.biradar@cimpress.com>
2022-12-13 05:42:59 +0000

    Moved chords from backbone to parrot

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/ping', (_, res) => res.send('pong'));
+router.use('/chords', require('./chord'));
 router.use('/messages', require('./message'));
 
 module.exports = router;
